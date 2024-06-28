859129a1494de72d4c85a99a804cbd50dfb463f7
zer0-u <mail@example.com>
2024-06-28 14:38:04 +0900

    連携用に設定を追加

--- client-vue/vite.config.js
+++ client-vue/vite.config.js
@@ -12,5 +12,15 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port:5174,
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '/'),
+      }
+    }
   }
 })
