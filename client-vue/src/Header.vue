224714b75a9febd4806b234fde999fdc43703a13
zer0-u <mail@example.com>
2024-06-28 14:53:30 +0900

    ヘッダを追加

---
+++ client-vue/src/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
