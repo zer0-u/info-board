871457f8bb71402fa4a8135a261aaf0fc2856752
zer0-u <mail@example.com>
2024-07-02 15:20:12 +0900

    Spring Securityの設定を一旦止める

--- server/src/main/java/com/example/InfoBoardApplication.java
+++ server/src/main/java/com/example/InfoBoardApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//　FIXME Spring Securityを利用する時にここも見直す
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class InfoBoardApplication {
 
 	public static void main(String[] args) {
