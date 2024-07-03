f840a80e0a6b75bc0c0612d79626ea9bb9b8ce2d
zer0-u <mail@example.com>
2024-07-03 15:20:35 +0900

    APIで取得した結果を表示するよう変更

--- client-vue/src/components/Article.vue
+++ client-vue/src/components/Article.vue
@@ -1,16 +1,14 @@
 <script setup>
-const article = {
-  title: 'タイトル',
-  date: 'YYYY/MM/DD',
-  author: '投稿した人',
-  content: 'これは本文です',
-};
+const props = defineProps({
+  article:Object
+});
+
 </script>
 
 <template>
   <h2>{{ article.title }}</h2>
   <div class="author">
-    <p>{{ article.date }}</p>
+    <p>{{ article.createdAt }}</p>
     <p>{{ article.author }}</p>
   </div>
   <main>
--- client-vue/src/views/ArticleView.vue
+++ client-vue/src/views/ArticleView.vue
@@ -1,11 +1,29 @@
 <script setup>
 
 import Article from "@/components/Article.vue";
+import {onMounted, ref} from "vue";
+
+const articles = ref([]);
+
+const fetchMessage = async () => {
+  try {
+    const response = await window.fetch("/api/articles");
+    articles.value = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(() => {
+  fetchMessage()
+});
 </script>
 
 <template>
   <h1>お知らせ一覧</h1>
-  <Article />
+  <div v-for="article of articles">
+    <Article v-bind:article/>
+  </div>
 </template>
 
 <style scoped>
