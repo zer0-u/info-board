e52017f9cf3d94cecc06b302508ff504ab4d6e18
zer0-u <mail@example.com>
2024-07-03 16:30:36 +0900

    見た目だけ作る

--- client-vue/src/views/PostView.vue
+++ client-vue/src/views/PostView.vue
@@ -4,6 +4,14 @@
 
 <template>
   <h1>お知らせを投稿する</h1>
+  <v-card variant="flat">
+    <v-card-text>
+      <v-text-field variant="outlined" label="タイトル"></v-text-field>
+      <v-text-field variant="outlined" label="投稿者名"></v-text-field>
+      <v-textarea variant="outlined" label="本文"></v-textarea>
+      <v-btn variant="outlined">投稿する</v-btn>
+    </v-card-text>
+  </v-card>
 </template>
 
 <style scoped>
