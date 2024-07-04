6d8f9fc9abd16fc59fadf0569ff26da877c9a85c
zer0-u <mail@example.com>
2024-07-04 16:34:43 +0900

    ダミーデータを使って一覧ページを整える

--- client-svelte/src/routes/articles/+page.svelte
+++ client-svelte/src/routes/articles/+page.svelte
@@ -1,1 +1,30 @@
+<script>
+    export let data;
+</script>
+
 <h1>お知らせ一覧</h1>
+
+<div>
+    {#each data.articles as article}
+        <div class="article">
+            <h2>{article.title}</h2>
+            <span class="author">
+                <p>{article.author}</p>
+                <p>{article.createdAt}</p>
+            </span>
+            <p>{article.content}</p>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .author {
+        color: darkgray;
+    }
+
+    .article {
+        border: 1px solid black;
+        padding: 1rem;
+        margin: 1rem;
+    }
+</style>
