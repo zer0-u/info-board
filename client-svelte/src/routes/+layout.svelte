8d745132247a59729bf7c23c555c170ed61b0833
zer0-u <mail@example.com>
2024-07-04 16:36:40 +0900

    全体のマージンもつけておく

--- client-svelte/src/routes/+layout.svelte
+++ client-svelte/src/routes/+layout.svelte
@@ -1,7 +1,15 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/articles">Articles</a>
-    <a href="/post">Post</a>
-</nav>
+<div class="base">
+    <nav>
+        <a href="/">Home</a> |
+        <a href="/articles">Articles</a> |
+        <a href="/post">Post</a>
+    </nav>
 
-<slot/>
+    <slot/>
+</div>
+
+<style>
+    .base {
+        margin: 3%;
+    }
+</style>
