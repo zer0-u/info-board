76c2109a2afb3a1bb6dfa4214ed455c99e62177c
zer0-u <mail@example.com>
2024-07-04 17:14:28 +0900

    APIアクセス部分を実装

--- client-svelte/src/routes/articles/+page.server.js
+++ client-svelte/src/routes/articles/+page.server.js
@@ -1,9 +1,7 @@
-export function load() {
+export async function load({fetch}) {
+    const response = await fetch('/api/articles');
+    const articles = await response.json();
     return {
-        articles: [
-            {title: 'title1', author: 'author1', content: 'content1', createdAt: "2024/09/01 09:30"},
-            {title: 'title2', author: 'author2', content: 'content2', createdAt: "2024/09/02 09:30"},
-            {title: 'title3', author: 'author3', content: 'content3', createdAt: "2024/09/03 09:30"},
-        ]
-    }
+        articles
+    };
 }
