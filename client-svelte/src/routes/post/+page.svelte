d314734fa565d09c4b163b1130f2a6664935ec9a
zer0-u <mail@example.com>
2024-07-08 15:41:32 +0900

    Toastの実装が面倒そうだったので簡単な形で代用

--- client-svelte/src/routes/post/+page.svelte
+++ client-svelte/src/routes/post/+page.svelte
@@ -2,11 +2,16 @@
     let title = '';
     let author = '';
     let content = '';
+    let completed = false;
 
     function reset() {
         title = '';
         author = '';
         content = '';
+    }
+
+    function changeStatus() {
+        completed = false;
     }
 
     async function submit() {
@@ -21,6 +26,7 @@
             body: JSON.stringify(article)
         });
         reset();
+        completed = true;
     }
 </script>
 
@@ -29,21 +35,24 @@
     <p>
         <label>
             タイトル
-            <input type="text" size="40" bind:value={title}/>
+            <input type="text" size="40" bind:value={title} on:input={changeStatus}/>
         </label>
     </p>
     <p>
         <label>
             投稿者名
-            <input type="text" size="40" bind:value={author}/>
+            <input type="text" size="40" bind:value={author} on:input={changeStatus}/>
         </label>
     </p>
     <p>
-        <textarea placeholder="本文" cols="60" rows="10" bind:value={content}/>
+        <textarea placeholder="本文" cols="60" rows="10" bind:value={content} on:input={changeStatus}/>
     </p>
     <button on:click={submit}>投稿する</button>
     <button on:click={reset}>リセットする</button>
 </form>
+{#if completed}
+    <p>投稿しました</p>
+{/if}
 
 <style>
 
