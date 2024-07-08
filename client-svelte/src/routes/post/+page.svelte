44c26c9c867f059abd08385c12b1865872401b39
zer0-u <mail@example.com>
2024-07-08 14:58:46 +0900

    とりあえず投稿フォームの形を作る

--- client-svelte/src/routes/post/+page.svelte
+++ client-svelte/src/routes/post/+page.svelte
@@ -1,1 +1,24 @@
 <h1>お知らせを投稿する</h1>
+<form>
+    <p>
+        <label>
+            タイトル
+            <input type="text" size="40"/>
+        </label>
+    </p>
+    <p>
+        <label>
+            投稿者名
+            <input type="text" size="40"/>
+        </label>
+    </p>
+    <p>
+        <textarea placeholder="本文" cols="60" rows="10"/>
+    </p>
+    <button>投稿する</button>
+    <button>リセットする</button>
+</form>
+
+<style>
+
+</style>
