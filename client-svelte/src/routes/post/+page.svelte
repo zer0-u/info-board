dc4b18b04a2dcb455496657ce524629e4b3a8bdd
zer0-u <mail@example.com>
2024-07-09 14:19:26 +0900

    バリデーション周りを一旦削除

--- client-svelte/src/routes/post/+page.svelte
+++ client-svelte/src/routes/post/+page.svelte
@@ -1,50 +1,5 @@
 <script>
-    let title = '';
-    let author = '';
-    let content = '';
-    let completed = false;
-    let hasError = false;
 
-    function reset() {
-        title = '';
-        author = '';
-        content = '';
-        hasError = false;
-    }
-
-    function setUncompleted() {
-        completed = false;
-        hasError = false;
-    }
-
-    function setCompleted() {
-        completed = true;
-        hasError = false;
-    }
-
-    async function submit() {
-        setUncompleted();
-        const article = {
-            title, author, content
-        }
-        if (!isValid(article)) {
-            hasError = true;
-            return;
-        }
-        await window.fetch('/api/articles', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(article)
-        });
-        reset();
-        setCompleted();
-    }
-
-    function isValid(article) {
-        return article.title !== '' && article.author !== '' && article.content !== '';
-    }
 </script>
 
 <h1>お知らせを投稿する</h1>
@@ -53,27 +8,21 @@
         <label>
             タイトル
             <input type="text" size="40" name="title" autocomplete="off"/>
-            {#if hasError && title === ""}<p class="error">必須入力です</p>{/if}
         </label>
     </p>
     <p>
         <label>
             投稿者名
             <input type="text" size="40" name="author" autocomplete="off"/>
-            {#if hasError && author === ""}<p class="error">必須入力です</p>{/if}
         </label>
     </p>
     <p>
         <textarea placeholder="本文" cols="60" rows="10" name="content" autocomplete="off"/>
-        {#if hasError && content === ""}<p class="error">必須入力です</p>{/if}
     </p>
     <button formaction="?/create">投稿する</button>
     <button formaction="?/reset">リセットする</button>
 </form>
 
 <style>
-    .error {
-        color: darkred;
-        font-size: 0.8em;
-    }
+
 </style>
