2d1c20d563fd8215243b67794f67d9f9e8ea56aa
zer0-u <mail@example.com>
2024-07-09 10:25:56 +0900

    サーバとの通信が失敗したらとりあえず0件取得と同じとみなす

--- client-svelte/src/routes/articles/+page.server.js
+++ client-svelte/src/routes/articles/+page.server.js
@@ -1,5 +1,8 @@
 export async function load({fetch}) {
     const response = await fetch('/api/articles');
+    if (response.status !== 200) {
+        return {articles: []};
+    }
     const articles = await response.json();
     return {
         articles
