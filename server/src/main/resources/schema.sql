978b58990e5e94792cdf7309b1154ccef707e81d
zer0-u <mail@example.com>
2024-07-10 14:03:58 +0900

    IDEAデフォルトのフォーマットに揃える

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -3,9 +3,9 @@ DROP TABLE IF EXISTS articles;
 
 CREATE TABLE articles
 (
-    id SERIAL PRIMARY KEY ,
-    author VARCHAR(256),
-    title VARCHAR(256),
+    id      SERIAL PRIMARY KEY,
+    author  VARCHAR(256),
+    title   VARCHAR(256),
     content TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
