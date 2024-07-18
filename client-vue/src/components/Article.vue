43f08ae65ace7d8a5d4a639c6af7b2617503a848
zer0-u <mail@example.com>
2024-07-18 14:05:05 +0900

    一覧に画像を表示(vue)

--- client-vue/src/components/Article.vue
+++ client-vue/src/components/Article.vue
@@ -15,6 +15,11 @@ const props = defineProps({
       <p>{{ dayjs(article.createdAt).format("YYYY/MM/DD HH:mm") }}</p>
     </v-card-subtitle>
     <v-card-text>{{ article.content }}</v-card-text>
+    <v-img v-for="image of article.images"
+           inline
+           width="50%"
+           :src="`${image.header},${image.base64}`"
+           :alt="image.name"/>
   </v-card>
 </template>
 
