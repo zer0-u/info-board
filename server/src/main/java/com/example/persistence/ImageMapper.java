6c348be3aebc36d446666df1b56e7b271ab13446
zer0-u <mail@example.com>
2024-07-19 17:12:04 +0900

    画像関係の試し実装を削除(Spring)

--- server/src/main/java/com/example/persistence/ImageMapper.java
+++ server/src/main/java/com/example/persistence/ImageMapper.java
@@ -2,7 +2,10 @@ package com.example.persistence;
 
 import com.example.persistence.entity.Image;
 import com.example.web.request.ImageRequest;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -11,16 +14,6 @@ public interface ImageMapper {
 
     @Select("SELECT id, name, header, base64 FROM images WHERE article_id = #{id}")
     List<Image> findByArticleId(int id);
-
-    @Select("SELECT id, article_id, name, header, base64 FROM images WHERE id = #{id}")
-    Image findById(int id);
-
-    @Select("SELECT id, name, header, base64 FROM images ORDER BY id")
-    List<Image> findAll();
-
-    @Insert("INSERT INTO images(name, header, base64) VALUES(#{name}, #{header}, #{base64})")
-    @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
-    void insert(Image image);
 
     @Insert("INSERT INTO images(article_id, name, header, base64) VALUES(#{articleId}, #{request.name}, #{request.header}, #{request.base64})")
     void save(@Param("request") ImageRequest request, @Param("articleId") int articleId);
