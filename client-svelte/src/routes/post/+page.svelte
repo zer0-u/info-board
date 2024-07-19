26ed07f78adccb4aa40d3000514ddb22ccd88b28
zer0-u <mail@example.com>
2024-07-19 13:49:45 +0900

    WIP 画像も投稿できるようにする(Svelte)

--- client-svelte/src/routes/post/+page.svelte
+++ client-svelte/src/routes/post/+page.svelte
@@ -1,5 +1,30 @@
 <script>
     export let form;
+    let imageFiles = [];
+    let base64Images = [];
+
+    async function readAsDataURL(imageFile) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(imageFile);
+            reader.onload = (event) => {
+                resolve(event.target.result);
+            };
+            reader.onerror = (event) => {
+                reject(new Error("画像の読み込みに失敗しました"));
+            };
+        });
+    }
+
+    async function encodeImageFiles() {
+        base64Images = await Promise.all(Array.from(imageFiles).map(async imageFile => {
+            const name = imageFile.name;
+            const [header, base64] = (await readAsDataURL(imageFile)).split(',');
+            return {name, header, base64};
+        }));
+        console.log(base64Images);
+    }
+
 </script>
 
 <h1>お知らせを投稿する</h1>
@@ -42,6 +67,15 @@
             </p>
         {/if}
     </p>
+    <p>
+        <input type="file" accept="image/*" name="imageFiles" multiple bind:files={imageFiles}
+               on:change={encodeImageFiles}/>
+    </p>
+    <ul>
+        {#each imageFiles as imageFile}
+            <li>{imageFile.name}</li>
+        {/each}
+    </ul>
     <button formaction="?/create">投稿する</button>
     <button formaction="?/reset">リセットする</button>
 </form>
