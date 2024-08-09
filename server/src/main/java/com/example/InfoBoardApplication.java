bc0abb70b5a18366f23f3c023ce93dde0bde6bed
zer0-u <mail@example.com>
2024-08-09 14:14:27 +0900

    Spring Securityについて学び始めた

--- server/src/main/java/com/example/InfoBoardApplication.java
+++ server/src/main/java/com/example/InfoBoardApplication.java
@@ -2,10 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-//　FIXME Spring Securityを利用する時にここも見直す
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class InfoBoardApplication {
 
 	public static void main(String[] args) {
