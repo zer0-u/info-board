139740bfd0f9bbf86fe8dda77ddd5e5f45ca4db3
zer0-u <mail@example.com>
2024-08-09 14:25:09 +0900

    テスト用に小さなコントローラを追加

---
+++ server/src/main/java/com/example/web/RootController.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    public String findAll() {
+        return "OK!";
+    }
+}
